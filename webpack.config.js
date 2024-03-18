c33df0185a369a17dc97f4ae5c6b480990d7497b
Ben DeMaso <108587665+MasoDay@users.noreply.github.com>
2024-03-18 17:48:23 -0600

    Git npm run build for webpack to work. Finally.

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,22 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    popup:  './src/popup.jsx',
+    popup:  './source/popup.jsx',
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js',
+  },
+  module: {
+    rules: [{
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+            options: {
+                presets: ["@babel/preset-env", "@babel/preset-react"],
+            }
+        }
+    }],
   },
 };
