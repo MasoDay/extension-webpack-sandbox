ee785cdf5e355c3ec37d40991406b766b2c2164c
Ben DeMaso <108587665+MasoDay@users.noreply.github.com>
2024-03-18 17:40:22 -0600

    Troubleshooting npm run build not working with webpack.

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-const btn = document.getElementById("btnClick");
-const image = document.getElementById("image")
+// const btn = document.getElementById("btnClick");
+// const image = document.getElementById("image")
 
-btn.addEventListener("click", function () {
-    fetch("https://dog.ceo/api/breeds/image/random")
-    .then(res => res.json())
-    .then(result => {
-        console.log(result)
-        image.src = result.message
-    })
-    .catch(error => console.log(error))
-})
+// btn.addEventListener("click", function () {
+//     fetch("https://dog.ceo/api/breeds/image/random")
+//     .then(res => res.json())
+//     .then(result => {
+//         console.log(result)
+//         image.src = result.message
+//     })
+//     .catch(error => console.log(error))
+// })
 
 // async function fetchData() {
 //     const url = 'https://api.thedogapi.com/v1/images/search';
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,11 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-    entry: {
-        popup: "./src/popup.jsx",
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: "[name].js",
-    },
-    module: {
-        rules: [{
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            use: {
-                loader: "babel-loader",
-                options: {
-                    presets: [["@babel/preset-env"], ["@babel/preset-react"]],
-                }
-            }
-        }],
-      },
+  entry: {
+    popup:  './src/popup.jsx',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
 };
